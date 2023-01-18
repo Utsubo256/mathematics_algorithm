a838dc62dd02d56c8963f7e4352a55d59574ed39
Utsubo256 <greeeentea256@gmail.com>
2023-01-18 22:05:20 +0900

    add task4.cpp

---
+++ tasks/task4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a1, a2, a3;
+  int result = 0;
+  cin >> a1 >> a2 >> a3;
+  result = a1 * a2 * a3;
+  cout << result << endl;
+  return 0;
+}
