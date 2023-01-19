354e81c6fb9726d16e5600f5387b588a9d324a12
Utsubo256 <greeeentea256@gmail.com>
2023-01-19 19:43:54 +0900

    add task9_TLE.cpp

---
+++ tasks/task9_TLE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long N, S, A[61];
+
+int main() {
+  cin >> N >> S;
+  for (int i = 1; i <= N; i++) cin >> A[i];
+  
+  for (long long i = 0; i < (1LL << N); i++) {
+    long long sum = 0;
+    for (int j = 1; j <= N; j++) {
+      if ((i & (1LL << (j-1))) != 0LL) sum+= A[j];
+    }
+    if (sum == S) { cout << "Yes" << endl; return 0; }
+  }
+  cout << "No" << endl;
+  return 0;
+}
