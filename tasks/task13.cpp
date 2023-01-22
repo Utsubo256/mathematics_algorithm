93f553c1645aa02d234b985483f6cdc08d644724
Utsubo256 <greeeentea256@gmail.com>
2023-01-22 17:39:21 +0900

    add task13.cpp

---
+++ tasks/task13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {  
+  long long N;
+  cin >> N;
+  for (long long i = 1; i * i <= N; i++) {
+    if (N % i == 0) {
+      cout << i << endl;
+      cout << N / i << endl;
+    }
+  }
+}
